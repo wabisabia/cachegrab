1a3cc22f5d713b6d97a0dbaccc752b8b95678cec
p4r17y817 <christian.dunn@warwick.ac.uk>
2021-03-05 20:44:44 +0000

    Add index wrapper for compile-time guarantees

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     let two_times_thunk_plus_three = || dcg.get(thunk2) + dcg.get(thunk3);
     let _ = dcg.thunk(&two_times_thunk_plus_three, &[thunk2, thunk3]);
 
-    dcg.set(a, 2).unwrap();
+    dcg.set(a, 2);
 
-    println!("{:?}", Dot::new(&*dcg.graph.borrow()));
+    println!("{:?}", Dot::new(&*dcg.borrow()));
 }
